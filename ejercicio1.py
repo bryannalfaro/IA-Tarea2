7d31e889e219986f8969c4220db7bc7cf114375c
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-02-11 16:12:26 -0600

    feat: leyendo txt desde consola y estado inicial y final

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,8 +9,18 @@
 import sys
 
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+    definicion_grafo = sys.argv[1]
+    estado_inicial = sys.argv[2]
+    estado_final = sys.argv[3]
 
-print('Hello')
+#Abrir archivo de definicion del grafo
+grafo = open(definicion_grafo, "r")
+vertices = grafo.readline()
+cantidad_aristas = grafo.readline()
+
+print(grafo.read())
+
+print('vertices: ',vertices)
+print('aristas: ',cantidad_aristas)
+print('estado inicial: ',estado_inicial)
+print('estado final: ',estado_final)
