3f33b37b5481697e74003110a6203b45fc76acdc
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-02-12 14:44:38 -0600

    feat: ejercicio 1 finalizado CoAutor> Raul

---
+++ dijkstra.py
@@ -0,0 +1,42 @@
+#Extraido de: https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
+import sys
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+    shortest_path = {}
+    previous_nodes = {}
+
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    shortest_path[start_node] = 0
+
+    while unvisited_nodes:
+        current_min_node = None
+        for node in unvisited_nodes:
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                previous_nodes[neighbor] = current_min_node
+
+        unvisited_nodes.remove(current_min_node)
+
+    return previous_nodes, shortest_path
+
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+    path.append(start_node)
+
+    print("Con un peso de: {}. La mejor ruta es: ".format(shortest_path[target_node]))
+    print(" ----> ".join(reversed(path)))
--- ejercicio1.py
+++ ejercicio1.py
@@ -7,7 +7,8 @@
 # Oscar Saravia
 
 import sys
-
+from grafo import *
+from dijkstra import *
 
 definicion_grafo = sys.argv[1]
 estado_inicial = sys.argv[2]
@@ -17,10 +18,32 @@ estado_final = sys.argv[3]
 grafo = open(definicion_grafo, "r")
 vertices = grafo.readline()
 cantidad_aristas = grafo.readline()
+aristas = []
+for arista in grafo.readlines():
+    arista = arista.split()
+    arista[0] = arista[0]
+    arista[1] = arista[1]
+    arista[2] = arista[2]
+    aristas.append(arista)
 
-print(grafo.read())
+nodes = []
+aristas_weighted = {}
 
-print('vertices: ',vertices)
-print('aristas: ',cantidad_aristas)
+for i in range(int(vertices)):
+    nodes.append(str(i+1))
+
+for node in nodes:
+    aristas_weighted[node] = {}
+
+for arista in aristas:
+    aristas_weighted[(arista[0])][arista[1]] = int(arista[2])
+
+graph = Graph(nodes, aristas_weighted)
+print('nodos: ',graph.get_nodes())
+print('cantidad de vertices: ',vertices)
+print('cantidad de aristas: ',cantidad_aristas)
 print('estado inicial: ',estado_inicial)
 print('estado final: ',estado_final)
+
+previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node=estado_inicial)
+print_result(previous_nodes, shortest_path, start_node=estado_inicial, target_node=estado_final)
